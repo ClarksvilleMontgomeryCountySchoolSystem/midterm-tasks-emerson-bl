b0c1924ccd4cc97e4175987173da4d0e52d527d0
emerson-bl <emerson.cook457@students.cmcss.net>
2025-12-17 14:02:23 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,13 +2,13 @@ slices = party_pizza_mini + large + medium
 print(f"Total number of slices: {slices}")
 
 people += 1
-share = (slices / people)
+share = (slices // people)
 leftover = (slices % people)
 print(f"Each person gets: {share}")
 print(f"Leftover slices: {leftover}")
 
 people += 2
-share = (slices / people)
+share = (slices // people)
 leftover = (slices % people)
 print(f"Each person gets: {share}")
 print(f"Leftover slices: {leftover}")
@@ -16,7 +16,7 @@ print(f"Leftover slices: {leftover}")
 #Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
 
 slices += 28
-share(slices / people)
+share(slices // people)
 leftover(slices % people)
 print(f"Each person gets: {share}")
 print(f"Leftover slices: {leftover}")
